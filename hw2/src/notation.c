2e5a3fbf3893c29c282f2afbbfd9bf3181d83d0c
Tracy Ho <trcho@cs.stonybrook.edu>
2021-03-11 01:21:50 -0500

    getopt

--- hw2/src/notation.c
+++ hw2/src/notation.c
@@ -35,6 +35,7 @@ the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#include <getopt.h>
 
 #include "chesstype.h"
 #include "notation.h"
@@ -1636,43 +1637,58 @@ int parse_options(argc,argv)
   dr->outfile = stdout;
   nb_move_to_dsp = 0;
 
-  while (narg < argc ) {
-    (void) strcpy (cp,argv[narg]);
-    switch (cp[0]) {
-    case '-' :
-      switch (cp[1]) {
+  static struct option long_options[] = {
+        {"long-algebraic", no_argument, 0, 'a'},
+        {"short-algebraic", no_argument, 0, 's' },
+        {"input-language", required_argument, 0, 'f' },
+        {"output-language", required_argument, 0, 't' },
+        {"output-file", required_argument, 0, 'o' },
+        {"show-after", required_argument, 0, 'c' },
+        {"end-after",required_argument, 0, 'e' },
+        {"board-only", no_argument, 0, 'b' },
+        {"driver",required_argument, 0, 'd' },
+        {"no-headers", no_argument, 0, 'i' },
+        {"help", no_argument, 0, 'h' },
+        {"version", no_argument, 0, 'v' },
+        {0,0,0,0}
+      };
+
+  int ch;
+
+  while ((ch = getopt_long(argc, argv, "asf:t:o:c:e:bd:ihv", long_options, &optind)) != EOF) {
+    switch (ch) {
       case 'f' : /* from langage */
-	if  ((narg+1) >= argc )
-	  fatal((stderr,"missing argument to %s option",cp));
-	narg++ ;
+	if  (!optarg )
+	  fatal((stderr,"missing argument to %s option",optarg));
+	// narg++ ;
 	in_language = find_keyword (t_language, NBLANGUAGES,
 				    DEFAULT_INPUT_LANGUAGE,
-				    argv[narg],TRUE);
+				    optarg,TRUE);
 	break;
       case 't' : /* to langage */
-	if  ((narg+1) >= argc )
-	  fatal((stderr,"missing argument to %s option",cp));
-	narg++ ;
+	if  (!optarg )
+	  fatal((stderr,"missing argument to %s option",optarg));
+	// narg++ ;
 	out_language = find_keyword (t_language, NBLANGUAGES,
 				     DEFAULT_OUTPUT_LANGUAGE,
-				     argv[narg],TRUE);
+				     optarg,TRUE);
 	break;
       case 'o' : /* next arg is output file */
-	narg++ ;
-	if ((dr->outfile = fopen (argv[narg],"w+")) == NULL) {
+	// narg++ ;
+	if ((dr->outfile = fopen (optarg,"w+")) == NULL) {
 	  (void) fprintf (stderr,"can't open %s output file\n",argv[narg]);
 	  (void) fprintf (stderr,"assume stdout for output\n");
 	}
   break;//TODO: put a break so it doesn't go to case 'e'
       case 'e':
-	if  ((narg+1) >= argc )
-	  fatal((stderr,"missing argument to %s option",cp));
-	narg++ ;
+	if  (!optarg )
+	  fatal((stderr,"missing argument to %s option",optarg));
+	// narg++ ;
 
 	i=0;
 	nb_move_to_dsp = 0;
 	move_to_display[nb_move_to_dsp] = 0;
-	while (isdigit(argv[narg][i])) {
+	while (isdigit(optarg[i])) {
 	  move_to_display[nb_move_to_dsp] =
 	    ((int) argv[narg][i] - (int) '0')
 	      + move_to_display[nb_move_to_dsp] * 10;
@@ -1682,16 +1698,16 @@ int parse_options(argc,argv)
 	stop_at_display = TRUE;
 	break;
       case 'c':
-	if  ((narg+1) >= argc )
-	  fatal((stderr,"missing argument to %s option",cp));
-	narg++ ;
+	if  (!optarg)
+	  fatal((stderr,"missing argument to %s option",optarg));
+	// narg++ ;
 
 	i=0;
-	while (isdigit(argv[narg][i])) {
+	while (isdigit(optarg[i])) {
 	  move_to_display[nb_move_to_dsp] = 0;
-	  while (isdigit(argv[narg][i])) {
+	  while (isdigit(optarg[i])) {
 	    move_to_display[nb_move_to_dsp] =
-	      ((int) argv[narg][i] - (int) '0')
+	      ((int) optarg[i] - (int) '0')
 	      + move_to_display[nb_move_to_dsp] * 10;
 	    i++;
 	  }
@@ -1701,7 +1717,7 @@ int parse_options(argc,argv)
 	    fatal((stderr,"max. number of move to display exceeded"));
 
 	  /* process next number */
-	  if (argv[narg][i] == ',')
+	  if (optarg[i] == ',')
 	    i++;
 	}
 	break;
@@ -1715,11 +1731,11 @@ int parse_options(argc,argv)
 	dr->only_board = TRUE;
 	break;
       case 'd': /* output driver */
-	if  ((narg+1) >= argc )
-	  fatal((stderr,"missing argument to %s option",cp));
+	if  (!optarg )
+	  fatal((stderr,"missing argument to %s option",optarg));
 	narg++ ;
 	driver = find_keyword(t_output, NB_DRIVER, DEFAULT_DRIVER,
-			      argv[narg],TRUE);
+			      optarg,TRUE);
 	break;
       case 'i': /* no headers */
 	dr->print_headers = FALSE;
@@ -1740,16 +1756,18 @@ int parse_options(argc,argv)
         }
          break;
       default:
-	error((stderr,"\nUnknown command line options %s\n",cp));
-	break;
-      }
-      break;
-    default: /* assume this is the input file */
-      if ((infile = fopen (cp,"r")) == NULL)
-	fatal((stderr,"can't open %s input file\n",cp));
-    }
-    narg++;
-  } /* process next arg */
+            error((stderr, "\nUnknown command line options %s\n", argv[optind]));
+            break;
+        }
+	// error((stderr,"\nUnknown command line options %s\n",cp));
+ //    default: /* assume this is the input file */
+ //      if ((infile = fopen (cp,"r")) == NULL)
+	// fatal((stderr,"can't open %s input file\n",cp));
+
+    // narg++;
+  }/* process next arg */
+  if ((infile = fopen(argv[optind], "r")) == NULL)
+    fatal((stderr, "can't open %s input file\n", argv[optind]));
   return(argc);
 }
 
