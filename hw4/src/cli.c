87027309f8702ad005435e8e49bbaaed666367b3
Tracy Ho <trcho@cs.stonybrook.edu>
2021-03-31 16:14:59 -0400

    skeleton

--- hw4/src/cli.c
+++ hw4/src/cli.c
@@ -11,13 +11,77 @@
 #include "conversions.h"
 #include "sf_readline.h"
 
+//added things
+#include <unistd.h>
+//define printer and job from imprimer.h
+typedef struct printer {
+
+}PRINTER;
+
+typedef struct job {
+
+}JOB;
+
 int quit = 0; //when to exit cli
 
 int run_cli(FILE *in, FILE *out)
 {
-    fprintf(out, "imp> ");
+	// int copy_stdin = dup(STDIN_FILENO);//copy_stdin is original
+	// dup2(fileno(in), STDIN_FILENO);
+	// dup2(copy_stdin, 0); //revert back to original
+
+	while(!quit){
+    	// char* inputcommand = sf_readline("imp> ");
+    	//convert char* to char **
+    	//free anything that is needed
+
+
+    }
+    // if(in== stdin) -> kill the processes
 
     abort();
 }
 
-//Commands are: help quit type printer conversion printers jobs print cancel disable enable pause resume
+//have a function that converts char* to char ** then call operation
+
+
+int operation(int num_args, char** arguments){
+	//first combine characters to arguments and keep track number of args
+	if(arguments[0] == NULL){
+		//throw an error;
+		return -1;
+	}
+	if(strcmp("help", arguments[0]) == 0){
+		if (num_args != 1){
+			//throw error
+			return -1;
+		}
+		//print help
+		//Commands are: help quit type printer conversion printers jobs print cancel disable enable pause resume
+		return 0;
+	}
+	if(strcmp("quit", arguments[0]) == 0){
+		if(num_args != 1){
+			//throw error
+			return -1;
+		}
+		quit = 1;
+		return 0;
+	}
+	if(strcmp("type", arguments[0]) == 0){
+		if(num_args != 2){
+			//throw error
+		}
+		FILE_TYPE *newtype = define_type(arguments[1]);
+		return 0;
+	}
+	if(strcmp("printer", arguments[0]) == 0){
+		if(num_args != 3){
+			//throw error
+			return -1;
+		}
+		//create printer (make status as idle)
+		return 0;
+	}
+
+}
