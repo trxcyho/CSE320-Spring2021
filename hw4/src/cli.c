bcbc9547b42d3254cb6fde553051a2a2e534d91f
Tracy Ho <trcho@cs.stonybrook.edu>
2021-04-01 16:57:11 -0400

    wokring help kind of

--- hw4/src/cli.c
+++ hw4/src/cli.c
@@ -13,26 +13,35 @@
 
 //added things
 #include <unistd.h>
+
+//protoype
+char** convert_to_commands(char* line);
+int operation(int num_args, char** arguments);
+
 //define printer and job from imprimer.h
 typedef struct printer {
 	//name of printer
 	//priner status
-	//next printer
 }PRINTER;
+//create array for printers
 
 typedef struct job {
 	//job id
-
+	//type of file
 	//job status
-	//next job
 }JOB;
+//create array for jobs
 
+int num_args = 0;
 int num_printers = 0;//make sure it dont go over 32 when adding
 int num_jobs = 0;//make sure it dont go over 64 when adding
 int quit = 0; //when to exit cli
 
+
+
 int run_cli(FILE *in, FILE *out)
 {
+	char **arguments;
 	if(in == NULL)
 		return -1;
 	//create a buffer to hold the storage of the lines
@@ -43,12 +52,19 @@ int run_cli(FILE *in, FILE *out)
 	}
 	else{
 		while(!quit){
-	    	// char* inputcommand = sf_readline("imp> ");
+	    	char* inputcommand = sf_readline("imp> ");
 			//if input command is EOF ->return -1//do we stop execution of processes?
+	    	arguments = convert_to_commands(inputcommand);
+	    	// printf("%s\n", arguments[0]);
+	    	operation(num_args, arguments);
 
 	    	//convert char* to char **
 	    	//free anything that is needed
 			//if quit is called return 0
+			for(int i = 0; i < num_args; i++)
+				free(arguments[i]);
+			free(arguments);
+			num_args = 0;
 	    }
 	}
 	if(in != stdin && quit)
@@ -58,7 +74,27 @@ int run_cli(FILE *in, FILE *out)
 }
 
 //have a function that converts char* to char ** then call operation
-
+char** convert_to_commands(char* line){
+	char *line_duplicate = malloc(strlen(line) + 1);
+	strcpy(line_duplicate, line);
+	char *word = strtok(line, " ");
+	while(word != NULL){
+		num_args++;
+		word = strtok(NULL, " ");
+	}
+	char **arguments = calloc(num_args, sizeof(char*));
+	word = strtok(line_duplicate, " ");
+	int index = 0;
+	while (word != NULL){
+		arguments[index] = calloc(strlen(word), sizeof(char*));
+		strcpy(arguments[index], word);
+		index++;
+		word = strtok(NULL, " ");
+	}
+	free(line);
+	free(line_duplicate);
+	return arguments;
+}
 
 int operation(int num_args, char** arguments){
 	//first combine characters to arguments and keep track number of args
@@ -72,6 +108,7 @@ int operation(int num_args, char** arguments){
 			return -1;
 		}
 		//print help
+		// printf("help");
 		//Commands are: help quit type printer conversion printers jobs print cancel disable enable pause resume
 		return 0;
 	}
@@ -80,6 +117,7 @@ int operation(int num_args, char** arguments){
 			//throw error
 			return -1;
 		}
+		// printf("quit");
 		quit = 1;
 		return 0;
 	}
@@ -95,7 +133,7 @@ int operation(int num_args, char** arguments){
 		if(num_args != 2){
 			//throw error
 		}
-		FILE_TYPE *newtype = define_type(arguments[1]);
+		// FILE_TYPE *newtype = define_type(arguments[1]);
 		return 0;
 	}
 	if(strcmp("printer", arguments[0]) == 0){
@@ -106,6 +144,8 @@ int operation(int num_args, char** arguments){
 		//create printer (make status as idle)
 		return 0;
 	}
+
+	//arg 1 is a number
 	if(strcmp("cancel", arguments[0]) == 0){
 
 	}
@@ -115,10 +155,20 @@ int operation(int num_args, char** arguments){
 	if (strcmp("resume", arguments[0]) == 0){
 
 	}
+
+	//arg 1 is a printer_name
 	if (strcmp("disable", arguments[0]) == 0){
 
 	}
 	if (strcmp("enable", arguments[0]) == 0){
+
+	}
+
+	//ubknown num_args associated
+	if(strcmp("print", arguments[0]) == 0){
+
+	}
+	if(strcmp("conversion", arguments[0]) == 0){
 
 	}
 
